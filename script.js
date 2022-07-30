73015597202469791a83869a4ed2be01a62b3fcb
cadbia <cadenbialow@icloud.com>
2022-07-30 16:41:09 -0400

    Button logic, Svg stuff

--- script.js
+++ script.js
@@ -1,32 +1,52 @@
-function updateSizes(){
+function updateSizes(table1, table2, befBut, aftBut){
     if(window.innerWidth >= window.innerHeight*2){
-        document.getElementById("table1").style.fontSize = "2vmin";
-        document.getElementById("table2").style.fontSize = "2vmin";
-        document.getElementById("bef_but").style.fontSize = "3vmin";
-        document.getElementById("aft_but").style.fontSize = "3vmin";
-        document.getElementById("bef_but").style.height = "10vmin";
-        document.getElementById("aft_but").style.height = "10vmin";
+        table1.style.fontSize = "2vmin";
+        table2.style.fontSize = "2vmin";
+        befBut.style.fontSize = "3vmin";
+        aftBut.style.fontSize = "3vmin";
+        befBut.style.height = "10vmin";
+        aftBut.style.height = "10vmin";
     }
     else if(window.innerWidth <= window.innerHeight){
-        document.getElementById("table1").style.fontSize = "0.8vmin";
-        document.getElementById("table2").style.fontSize = "0.8vmin";
-        document.getElementById("bef_but").style.fontSize = "2vmin";
-        document.getElementById("aft_but").style.fontSize = "2vmin";
-        document.getElementById("bef_but").style.height = "5vmin";
-        document.getElementById("aft_but").style.height = "5vmin";
+        table1.style.fontSize = "0.8vmin";
+        table2.style.fontSize = "0.8vmin";
+        befBut.style.fontSize = "2vmin";
+        aftBut.style.fontSize = "2vmin";
+        befBut.style.height = "5vmin";
+        aftBut.style.height = "5vmin";
     }
     else{
-        document.getElementById("table1").style.fontSize = "1.2vmax";
-        document.getElementById("table2").style.fontSize = "1.2vmax";
-        document.getElementById("bef_but").style.fontSize = "2vmax";
-        document.getElementById("aft_but").style.fontSize = "2vmax";
-        document.getElementById("bef_but").style.height = "5.5vmax";
-        document.getElementById("aft_but").style.height = "5.5vmax";
+        table1.style.fontSize = "1.2vmax";
+        table2.style.fontSize = "1.2vmax";
+        befBut.style.fontSize = "2vmax";
+        aftBut.style.fontSize = "2vmax";
+        befBut.style.height = "5.5vmax";
+        aftBut.style.height = "5.5vmax";
     }
 }
-addEventListener('load', (event) => {
-    updateSizes();
-    setInterval(updateSizes, 500)
+let isAfter = false;
+function before(){
+    const circle1 = document.getElementById("circle1");
+        if(isAfter === true){
+            isAfter = false;
+            circle1.setAttribute("fill", "#F9975D");
+        }
+}
+function after(){
+    const circle1 = document.getElementById("circle1");
+    if(isAfter === false){
+        isAfter = true;
+        circle1.setAttribute("fill", "#B2EA70");
+    }
+}
+
+addEventListener('load', () => {
+    const table1 = document.getElementById("table1");
+    const table2 = document.getElementById("table2");
+    const befBut = document.getElementById("bef_but");
+    const aftBut = document.getElementById("aft_but");
+    updateSizes(table1,table2,befBut,aftBut)
+    setInterval(updateSizes(table1,table2,befBut,aftBut), 500);
 });
 
 
