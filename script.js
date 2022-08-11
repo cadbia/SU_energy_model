b44a5b10589066401666663509ff400b6e67b60d
cadbia <cadenbialow@icloud.com>
2022-08-11 15:50:17 -0400

    Google Maps Background

--- script.js
+++ script.js
@@ -61,26 +61,25 @@ addEventListener('load', () => {
     });
 });
 
+addEventListener('load', () => {
+var pt = document.getElementById("svgDoc").createSVGPoint();
+
+function alert_coords(evt) {
+    pt.x = evt.clientX;
+    pt.y = evt.clientY;
+
+    // The cursor point, translated into svg coordinates
+    var cursorpt =  pt.matrixTransform(document.getElementById("svgDoc").getScreenCTM().inverse());
+    console.log("(" + cursorpt.x.toFixed(1) + ", " + cursorpt.y.toFixed(1) + ")");
+}
+
+document.getElementById("svgDoc").addEventListener("mousedown", alert_coords, false);
+
+});
+
 // addEventListener('load', () => {
 //     const table1 = document.getElementById("table1");
 //     const table2 = document.getElementById("table2");
 //     updateSizes(table1,table2)
 //     setInterval(updateSizes(table1,table2), 500);
 // });
-
-// addEventListener('load', () => {
-// var pt = document.getElementById("svgDoc").createSVGPoint();
-
-// function alert_coords(evt) {
-//     pt.x = evt.clientX;
-//     pt.y = evt.clientY;
-
-//     // The cursor point, translated into svg coordinates
-//     var cursorpt =  pt.matrixTransform(document.getElementById("svgDoc").getScreenCTM().inverse());
-//     console.log("(" + cursorpt.x.toFixed(1) + ", " + cursorpt.y.toFixed(1) + ")");
-// }
-
-// document.getElementById("svgDoc").addEventListener("mousedown", alert_coords, false);
-
-// });
-
