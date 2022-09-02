96a2ca4f2a0b5b6f45699df4495288478d149207
cadbia <cadenbialow@icloud.com>
2022-09-02 16:51:26 -0400

    Comments added

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # SU_energy_model
 South campus data visualization
+
+To make edits to this repository begin by making your own copy by forking the project. You can fork this repository by hitting the fork button in the top-right of this page. 
+
+Then clone the repository in your preferred IDE. Cloning the repository will link your editor to the repository. See the tutorial below for help. 
+
+https://docs.github.com/en/repositories/creating-and-managing-repositories/cloning-a-repository
+
+Now you will be able to use the CLI to push your code to your repository. This looks something like this: 
+
+git add .
+git commit -a -m "commit message" (do not need commit message either, although helpful)
+git push
+
+Below is an explanation of how each file contributes to the overall website. 
+
+index.html
+
+--> Contains the structure of the webpage
+--> Responsible for the initial state of the webpage
+--> Contains Buttons, images, and SVG paths. Also contains links to stylesheet and JavaScript files 
+
+script.js
+
+--> Responsible for making the site responsive
+--> Functions are pointed to and executed by event listeners in HTML code or JS code
+--> Code can dynamically change the color, size, visibility, etc. of HTML elements
+
+style.css
+
+--> Responsible for initial style of elements withing HTML page
+--> Styling attributes can include color, size, visibility, etc.
+--> Class and id are used to select which HTML elements are being styled 
+
+
+composer.json and index.php 
+
+--> signal that this site should be hosted as a PHP project
+--> This is required because this site does not contain a backend 
+--> These files DO NOT need to be edited
+
+This website can be easily hosted on Heroku when you are satisfied with your local changes and wish to push them to a public link.
+
+Please follow this tutorial to link cloned repository to a Heroku server.  
+
+https://devcenter.heroku.com/articles/github-integration. 
--- script.js
+++ script.js
@@ -1,220 +1,33 @@
-// function updateSizes(table1, table2){
-//     if(window.innerWidth >= window.innerHeight*2){
-//         table1.style.fontSize = "1.7vmin";
-//         table2.style.fontSize = "1.7vmin"
-//     }
-//     else if(window.innerWidth <= window.innerHeight){
-//         table1.style.fontSize = "0.8vmin";
-//         table2.style.fontSize = "0.8vmin";
-//     }
-//     else{
-//         table1.style.fontSize = "1.1vmax";
-//         table2.style.fontSize = "1.1vmax";
-//     }
-// }
-let mode = "EUI";
-let isAfter = false;
-function before(){
-    var aftBut = document.getElementById("aft_but");
-    var befBut = document.getElementById("bef_but")
-    aftBut.style.color = "black";
-    var slides = document.getElementsByClassName("rectangle");
-    if(isAfter === true && mode === "EUI"){
-        befBut.style.color = "#B2EA70";
-        isAfter = false;
-        for (var i = 0; i < slides.length-16; i++) {
-            slides.item(i).setAttribute("fill", "url('#myGradientBefPoor')");
-            }
-        for (var i = 10; i < slides.length; i++) {       
-                slides.item(i).setAttribute("fill", "url('#myGradientBefMild')");
-        }
-    }
-    if(isAfter === true && mode === "comfort"){
-        befBut.style.color = "#f4e9dd";
-        isAfter = false;
-        for (var i = 0; i < slides.length; i++) {
-                slides.item(i).setAttribute("fill", "url('#comfortBefPoor')");
-        }
-    }
-    if(isAfter === true && mode === "IAQ"){
-        befBut.style.color = "#c2eaf6";
-        isAfter = false;
-        for (var i = 0; i < slides.length-16; i++) {
-            slides.item(i).setAttribute("fill", "url('#iaqBefMild')");
-            }
-        for (var i = 10; i < slides.length; i++) {       
-                slides.item(i).setAttribute("fill", "url('#iaqBefPoor')");
-        }
-    }
-}
-function after(){
-    var aftBut = document.getElementById("aft_but");
-    var befBut = document.getElementById("bef_but");
-    befBut.style.color = "black";
-    var slides = document.getElementsByClassName("rectangle");
-    if(isAfter === false && mode === "EUI"){
-        aftBut.style.color = "#C85C5C";
-        isAfter = true;
-        for (var i = 0; i < slides.length-16; i++) {
-            slides.item(i).setAttribute("fill", "url('#myGradientAftGood')");
-            }
-        for (var i = 10; i < slides.length; i++) {       
-                slides.item(i).setAttribute("fill", "url('#myGradientAftExcellent')");
-        }
-    }
-    if(isAfter === false && mode === "comfort"){
-        aftBut.style.color = "#C85C5C";
-        isAfter = true;
-        for (var i = 0; i < slides.length; i++) {
-                slides.item(i).setAttribute("fill", "url('#comfortAftExcellent')");
-        }
-    }
-    if(isAfter === false && mode === "IAQ"){
-        aftBut.style.color = "#ccc0bb";
-        isAfter = true;
-        for (var i = 0; i < slides.length-16; i++) {
-            slides.item(i).setAttribute("fill", "url('#iaqAftExcellent')");
-            }
-        for (var i = 10; i < slides.length; i++) {       
-                slides.item(i).setAttribute("fill", "url('#iaqAftGood')");
-        }
-    }
-    
-}
-function comfort(){
-    var euiBut = document.getElementById("EUI_but");
-    var comfortBut = document.getElementById("comfort_but");
-    var aiqBut = document.getElementById("IAQ_but");
-    aiqBut.style.color = "black";
-    comfortBut.style.color = "white";
-    euiBut.style.color = "black";
-    var slides = document.getElementsByClassName("rectangle");
-    var aftBut = document.getElementById("aft_but");
-    var befBut = document.getElementById("bef_but");
-    var euiBar = document.getElementById("EUI_bar");
-    var comfortBar = document.getElementById("comfort_bar");
-    var iaqBar = document.getElementById("IAQ_bar");
-    if (mode != "comfort"){
-        mode = "comfort";
-        iaqBar.style.display = "none";
-        euiBar.style.display = "none";
-        comfortBar.style.display = "block";
-        befBut.style.backgroundColor = "#ee817f";
-        aftBut.style.backgroundColor = "#f4e9dd";
-    }
-    if(isAfter === false){
-        befBut.style.color = "#f4e9dd";
-        for (var i = 0; i < slides.length; i++) {
-            slides.item(i).setAttribute("fill", "url('#comfortBefPoor')");
-        }
-    }
-    else{
-        aftBut.style.color = "#C85C5C";
-        for (var i = 0; i < slides.length; i++) {
-            slides.item(i).setAttribute("fill", "url('#comfortAftExcellent')");
-        }
-    }
-    
-}
-function eui(){
-    var euiBut = document.getElementById("EUI_but");
-    var comfortBut = document.getElementById("comfort_but");
-    var aiqBut = document.getElementById("IAQ_but");
-    aiqBut.style.color = "black";
-    comfortBut.style.color = "black";
-    euiBut.style.color = "white";
-    var slides = document.getElementsByClassName("rectangle");
-    var aftBut = document.getElementById("aft_but");
-    var befBut = document.getElementById("bef_but");
-    var euiBar = document.getElementById("EUI_bar");
-    var comfortBar = document.getElementById("comfort_bar");
-    var iaqBar = document.getElementById("IAQ_bar");
-    if (mode != "EUI"){
-        mode = "EUI";
-        iaqBar.style.display = "none";
-        euiBar.style.display = "block";
-        comfortBar.style.display = "none";
-        befBut.style.backgroundColor = "#C85C5C";
-        aftBut.style.backgroundColor = "#B2EA70";
-    }
-    if(isAfter === false){
-    befBut.style.color = "#B2EA70";
-       for (var i = 0; i < slides.length-16; i++) {
-            slides.item(i).setAttribute("fill", "url('#myGradientBefPoor')");
-            }
-        for (var i = 10; i < slides.length; i++) {       
-                slides.item(i).setAttribute("fill", "url('#myGradientBefMild')");
-        } 
-    }
-    else{
-        aftBut.style.color = "#C85C5C";
-        for (var i = 0; i < slides.length-16; i++) {
-            slides.item(i).setAttribute("fill", "url('#myGradientAftGood')");
-            }
-        for (var i = 10; i < slides.length; i++) {       
-                slides.item(i).setAttribute("fill", "url('#myGradientAftExcellent')");
-        }
-    }
-}
-function iaq(){
-    var iaqBut = document.getElementById("IAQ_but");
-    var euiBut = document.getElementById("EUI_but");
-    var comfortBut = document.getElementById("comfort_but");
-    iaqBut.style.color = "white";
-    comfortBut.style.color = "black";
-    euiBut.style.color = "white";
-    var slides = document.getElementsByClassName("rectangle");
-    var aftBut = document.getElementById("aft_but");
-    var befBut = document.getElementById("bef_but");
-    var euiBar = document.getElementById("EUI_bar");
-    var comfortBar = document.getElementById("comfort_bar");
-    var iaqBar = document.getElementById("IAQ_bar");
-    if (mode != "IAQ"){
-        mode = "IAQ";
-        iaqBar.style.display = "block";
-        euiBar.style.display = "none";
-        comfortBar.style.display = "none";
-        befBut.style.backgroundColor = "#ccc0bb";
-        aftBut.style.backgroundColor = "#c2eaf6";
-    }
-    if(isAfter === false){
-        befBut.style.color = "#c2eaf6";
-        for (var i = 0; i < slides.length-16; i++) {
-            slides.item(i).setAttribute("fill", "url('#iaqBefMild')");
-            }
-        for (var i = 10; i < slides.length; i++) {       
-                slides.item(i).setAttribute("fill", "url('#iaqBefPoor')");
-        }
-    }
-    else{
-        aftBut.style.color = "#ccc0bb";
-        for (var i = 0; i < slides.length-16; i++) {
-            slides.item(i).setAttribute("fill", "url('#iaqAftExcellent')");
-            }
-        for (var i = 10; i < slides.length; i++) {       
-                slides.item(i).setAttribute("fill", "url('#iaqAftGood')");
-        }
-    }
-    
-}
-
+//Function executed when all elements are loaded on page
 addEventListener('load', () => {
-
-    const hiddenEl = document.getElementById('popup');
-    const table = document.getElementById('table');
-    const poly25 = document.getElementById('polygon25');
-    const poly26 = document.getElementById('polygon26');
-
-    const slides = document.getElementsByClassName('rectangle');
-
-    for (const slide of slides) {
-        slide.addEventListener('mouseover', function handleMouseOver(){
+    //Ensureses DOM is loaded
+    //Then accesses DOM elements and stores as global variables for use in all functions
+    hiddenEl = document.getElementById('popup');
+    buildings = document.getElementsByClassName('rectangle');
+    iaqBut = document.getElementById("IAQ_but");
+    euiBut = document.getElementById("EUI_but");
+    comfortBut = document.getElementById("comfort_but");
+    aftBut = document.getElementById("aft_but");
+    befBut = document.getElementById("bef_but");
+    euiBar = document.getElementById("EUI_bar");
+    comfortBar = document.getElementById("comfort_bar");
+    iaqBar = document.getElementById("IAQ_bar");
+    //Adds a "mouseover" event listener to each building to display data table (hiddenEl). 
+    for (const building of buildings) {
+        building.addEventListener('mouseover', function handleMouseOver(){
             hiddenEl.style.display = 'flex';
         });
-        slide.addEventListener('mouseout', function handleMouseOut(){
+        building.addEventListener('mouseout', function handleMouseOut(){
             hiddenEl.style.display = 'none';
         });
     }
+    
+//** Uncomment this code if **
+
+    //you wish to only diplay table on hover of buildings where data was collected
+
+    // const poly25 = document.getElementById('polygon25');
+    // const poly26 = document.getElementById('polygon26');
 
     // poly25.addEventListener('mouseover', function handleMouseOver() {
     //     hiddenEl.style.display = 'flex';
@@ -228,21 +41,200 @@ addEventListener('load', () => {
     // poly26.addEventListener('mouseout', function handleMouseOut() {
     //     hiddenEl.style.display = 'none';
     // });
+
+//** Uncomment this code if **
+
+    //You want to get the coodinates of your mouse click anywhere on the svg canvas
+    //These coordinates will be logged in the console of the webpage
+    //These points can then be used to redefine/add building olines for the svg polygons in index.html
+
+    // var pt = document.getElementById("svgDoc").createSVGPoint();
+
+    // function alert_coords(evt) {
+    //     pt.x = evt.clientX;
+    //     pt.y = evt.clientY;
+
+    //     // The cursor point, translated into svg coordinates
+    //     var cursorpt =  pt.matrixTransform(document.getElementById("svgDoc").getScreenCTM().inverse());
+    //     console.log("(" + cursorpt.x.toFixed(1) + ", " + cursorpt.y.toFixed(1) + ")");
+    // }
+
+    // document.getElementById("svgDoc").addEventListener("mousedown", alert_coords, false);
+
 });
 
-// addEventListener('load', () => {
-// var pt = document.getElementById("svgDoc").createSVGPoint();
+//Global variable declartions
+let mode = "EUI";
+let isAfter = false;
 
-// function alert_coords(evt) {
-//     pt.x = evt.clientX;
-//     pt.y = evt.clientY;
+//Funtion that runs onclick of before button
+function before(){
+    //Ensures that current state is after prior to executing rest
+    if(isAfter === true){
+        // changes after button text to black to signify unclicked
+        aftBut.style.color = "black";
+        //Bef button text color is set to backgorund color of after to show clicked
+        befBut.style.color = aftBut.style.backgroundColor;
+        //Now that bef has been clicked, isAfter must be set to false
+        isAfter = false;
+        //If mode is currently EUI, "before" EUI fills for buildings are displayed
+        if(mode === "EUI"){
+            for (var i = 0; i < buildings.length-16; i++) {
+                buildings.item(i).setAttribute("fill", "url('#myGradientBefPoor')");
+                }
+            for (var i = 10; i < buildings.length; i++) {       
+                buildings.item(i).setAttribute("fill", "url('#myGradientBefMild')");
+            }
+        }
+        //If mode is currently comfort, "before" comfort fills for buildings are displayed
+        if(mode === "comfort"){
+            for (var i = 0; i < buildings.length; i++) {
+                buildings.item(i).setAttribute("fill", "url('#comfortBefPoor')");
+            }
+        }
+        //If mode is currently IAQ, "before" IAQ fills for buildings are displayed
+        if(mode === "IAQ"){
+            for (var i = 0; i < buildings.length-16; i++) {
+                buildings.item(i).setAttribute("fill", "url('#iaqBefMild')");
+                }
+            for (var i = 10; i < buildings.length; i++) {       
+                buildings.item(i).setAttribute("fill", "url('#iaqBefPoor')");
+            }
+        }
+    } 
+}
 
-//     // The cursor point, translated into svg coordinates
-//     var cursorpt =  pt.matrixTransform(document.getElementById("svgDoc").getScreenCTM().inverse());
-//     console.log("(" + cursorpt.x.toFixed(1) + ", " + cursorpt.y.toFixed(1) + ")");
-// }
+//Funtion that runs onclick of before button
+function after(){
+    //Ensures that current state is before prior to executing rest
+    if(isAfter === false){
+        // changes before button text to black to signify unclicked
+        befBut.style.color = "black";
+        //Aft button text color is set to backgorund color of bef to show clicked
+        aftBut.style.color = befBut.style.backgroundColor;
+        //Now that aft has been clicked, isAfter must be set to true
+        isAfter = true;
+        //If mode is currently EUI, "after" EUI fills for buildings are displayed
+        if(mode === "EUI"){
+            aftBut.style.color = "#C85C5C";
+            for (var i = 0; i < buildings.length-16; i++) {
+                buildings.item(i).setAttribute("fill", "url('#myGradientAftGood')");
+                }
+            for (var i = 10; i < buildings.length; i++) {       
+                    buildings.item(i).setAttribute("fill", "url('#myGradientAftExcellent')");
+            }
+        }
+        //If mode is currently comfort, "after" comfort fills for buildings are displayed
+        if(mode === "comfort"){
+            for (var i = 0; i < buildings.length; i++) {
+                    buildings.item(i).setAttribute("fill", "url('#comfortAftExcellent')");
+            }
+        }
+        //If mode is currently IAQ, "after" IAQ fills for buildings are displayed
+        if(mode === "IAQ"){
+            for (var i = 0; i < buildings.length-16; i++) {
+                buildings.item(i).setAttribute("fill", "url('#iaqAftExcellent')");
+                }
+            for (var i = 10; i < buildings.length; i++) {       
+                    buildings.item(i).setAttribute("fill", "url('#iaqAftGood')");
+            }
+        }
+    }      
+}
 
-// document.getElementById("svgDoc").addEventListener("mousedown", alert_coords, false);
+//Function executed onClick of euiBut
+function eui(){
+    //Changes "mode" button colors to show that EUI has been clicked
+    iaqBut.style.color = "black";
+    comfortBut.style.color = "black";
+    euiBut.style.color = "white";
+    //Ensures that current state is not equal to "EUI" prior to executing rest
+    if (mode != "EUI"){
+        //Sets mode to comfort now that it has been clicked
+        mode = "EUI";
+        //Changes visibility of vis keys to ensure euiBar is displayed
+        iaqBar.style.display = "none";
+        euiBar.style.display = "block";
+        comfortBar.style.display = "none";
+        //Sets background colors of before and after buttons to match "EUI" mode
+        befBut.style.backgroundColor = "#C85C5C";
+        aftBut.style.backgroundColor = "#B2EA70";
+    }
+    //If it isAfter is false. Then onclick of euiBut must dispaly before function.
+    //However to ensure before function executes, isAfter must be set to true. 
+    if(isAfter === false){
+        isAfter = true; 
+        before();
+    }
+    //If it isAfter is true. Then onclick of euiBut must display after function.
+    //However to ensure after function executes, isAfter must be set to false.
+    else{
+        isAfter = false; 
+        after();
+    }
+}
 
-// });
+//Function executed onClick of iaqBut
+function iaq(){
+    //Changes "mode" button colors to show that IAQ has been clicked
+    iaqBut.style.color = "white";
+    comfortBut.style.color = "black";
+    euiBut.style.color = "white";
+    //Ensures that current state is not equal to "EUI" prior to executing rest
+    if (mode != "IAQ"){
+        //Sets mode to comfort now that it has been clicked
+        mode = "IAQ";
+        //Changes visibility of vis keys to ensure iaqBar is displayed
+        iaqBar.style.display = "block";
+        euiBar.style.display = "none";
+        comfortBar.style.display = "none";
+        //Sets background colors of before and after buttons to match "EUI" mode
+        befBut.style.backgroundColor = "#ccc0bb";
+        aftBut.style.backgroundColor = "#c2eaf6";
+    }
+    //If it isAfter is false. Then onclick of iaqBut must display before function.
+    //However to ensure before function executes, isAfter must be set to true. 
+    if(isAfter === false){
+        isAfter = true; 
+        before();
+    }
+    //If it isAfter is true. Then onclick of iaqBut must dispaly after function.
+    //However to ensure after function executes, isAfter must be set to false.
+    else{
+        isAfter = false; 
+        after();
+    }  
+}
 
+//Function executed onClick of comfortBut
+function comfort(){ 
+    //Changes "mode" button colors to show that comfort has been clicked
+    iaqBut.style.color = "black";
+    comfortBut.style.color = "white";
+    euiBut.style.color = "black";
+    //Ensures that current state is not equal to "comfort" prior to executing rest
+    if (mode != "comfort"){
+        //Sets mode to comfort now that it has been clicked
+        mode = "comfort";
+        //Changes visibility of vis keys to ensure comfortBar is displayed
+        iaqBar.style.display = "none";
+        euiBar.style.display = "none";
+        comfortBar.style.display = "block";
+        //Sets background colors of before and after buttons to match "comfort" mode
+        befBut.style.backgroundColor = "#ee817f";
+        aftBut.style.backgroundColor = "#f4e9dd";
+    }
+    //If it isAfter is false. Then onclick of comfortBut must dispaly before function.
+    //However to ensure before function executes, isAfter must be set to true. 
+    if(isAfter === false){
+        isAfter = true; 
+        before();
+    }
+    //If it isAfter is true. Then onclick of comfortBut must display after function.
+    //However to ensure after function executes, isAfter must be set to false.
+    else{
+        isAfter = false; 
+        after();
+    }
+    
+}
